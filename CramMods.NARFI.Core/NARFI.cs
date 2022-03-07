cf02ec08399dc5e435bc7f27deb9af15a26c551d
Cram42 <cram42@users.noreply.github.com>
2022-03-07 09:06:23 +0800

    Calculate fields and getters once

--- CramMods.NARFI.Core/NARFI.cs
+++ CramMods.NARFI.Core/NARFI.cs
@@ -9,6 +9,9 @@ namespace CramMods.NARFI
 {
     public class NARFI : IPluginRegistry, IFieldValueGetter
     {
+        private List<Field> _fields = new();
+        private List<IFieldValueGetter> _getters = new();
+
         private IGameEnvironmentState _state;
         public NARFI(IGameEnvironmentState state)
         {
@@ -21,10 +24,21 @@ namespace CramMods.NARFI
         {
             plugin.SetGameEnvironmentState(_state);
             _plugins.Add(plugin);
+            RefreshPlugins();
         }
         public void UnregisterPlugin(IPlugin plugin) => _plugins.Remove(plugin);
 
-        private List<IFieldValueGetter> _getters => _plugins.SelectMany(p => p.Getters).ToList();
+        private void RefreshPlugins()
+        {
+            _fields = new();
+            _getters = new();
+            foreach (IPlugin plugin in _plugins)
+            {
+                _fields.AddRange(plugin.Fields);
+                _getters.AddRange(plugin.Getters);
+            }
+        }
+
         public bool CanGet(IMajorRecordGetter record, Field field) => _getters.Any(getter => getter.CanGet(record, field));
         public IFieldValue? Get(IMajorRecordGetter record, Field field, FieldPath? remainingPath) => _getters.FirstOrDefault(getter => getter.CanGet(record, field))?.Get(record, field, remainingPath) ?? throw new NotImplementedException("No getter for this operation");
         public IFieldValue? Get(IMajorRecordGetter record, FieldPath path) => Get(record, path.Dequeue(), path);
