df0f835b3eb98e2bf397bfa35a95448087e92e74
Cram42 <cram42@users.noreply.github.com>
2022-03-07 14:26:34 +0800

    Implement RaceGroups

---
+++ CramMods.NARFI.Skyrim/Skyrim/RaceGroup.cs
@@ -0,0 +1,62 @@
+﻿using Mutagen.Bethesda.Skyrim;
+
+namespace CramMods.NARFI.Skyrim
+{
+    public class RaceGroup
+    {
+        private string _name;
+        public string Name => _name;
+
+        private List<IRaceGetter> _races = new();
+        public IReadOnlyList<IRaceGetter> Races => _races.AsReadOnly();
+
+        public IReadOnlyList<string> RaceIds => _races
+            .Select(r => r.EditorID)
+            .Where(n => n != null)
+            .Select(r => r!)
+            .ToList().AsReadOnly();
+
+        public RaceGroup(string name, IEnumerable<IRaceGetter> races) => (_name, _races) = (name, races.ToList());
+        public RaceGroup(string name, params IRaceGetter[] races) : this(name, races.ToList()) { }
+
+        public bool Contains(IRaceGetter race) => _races.Any(r => r.EditorID?.Equals(race.EditorID, StringComparison.InvariantCultureIgnoreCase) ?? false);
+        public bool Contains(string raceId) => RaceIds.Any(n => n.Equals(raceId, StringComparison.InvariantCultureIgnoreCase));
+
+        public override string ToString() => $"{_name} [{_races.Count}]";
+
+
+        public static IEnumerable<RaceGroup> FromIdDictionary(IDictionary<string, string[]> raceIdDict, IEnumerable<IRaceGetter> allRaces)
+        {
+            List<RaceGroup> raceGroups = new();
+            foreach (KeyValuePair<string, string[]> entry in raceIdDict) raceGroups.Add(FromIds(entry.Key, entry.Value, raceIdDict, allRaces));
+            return raceGroups;
+        }
+
+        public static RaceGroup FromIds(string name, string[] names, IDictionary<string, string[]> raceIdDict, IEnumerable<IRaceGetter> allRaces)
+        {
+            Dictionary<string, string[]> others = new(raceIdDict, StringComparer.InvariantCultureIgnoreCase);
+            List<IRaceGetter> outputRaces = new();
+
+            foreach (string rname in names)
+            {
+                if (others.ContainsKey(rname))
+                {
+                    RaceGroup otherGroup = FromIds(rname, others[rname], raceIdDict, allRaces);
+                    outputRaces.AddRange(otherGroup.Races);
+                } 
+                else
+                {
+                    IRaceGetter? race = allRaces.FirstOrDefault(race => race.EditorID?.Equals(rname, StringComparison.InvariantCultureIgnoreCase) ?? false);
+                    if (race != null) outputRaces.Add(race);
+                }
+            }
+
+            outputRaces = outputRaces
+                .DistinctBy(race => race.FormKey.ToString())
+                .OrderBy(race => race.FormKey.ToString())
+                .ToList();
+
+            return new(name, outputRaces);
+        }
+    }
+}
---
+++ Tests/Skyrim/RaceGroupTests.cs
@@ -0,0 +1,48 @@
+﻿using CramMods.NARFI.Skyrim;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Environments;
+using Mutagen.Bethesda.Skyrim;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CramMods.NARFI.Tests.Skyrim
+{
+    [TestClass]
+    public class RaceGroupTests
+    {
+        private IGameEnvironmentState<ISkyrimMod, ISkyrimModGetter> _state = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimSE);
+        private Dictionary<string, string[]> raceIdGroups = new()
+        {
+            { "Argonian", new[] { "ArgonianRace", "ArgonianRaceVampire" } },
+            { "Breton", new[] { "BretonRace", "BretonRaceVampire" } },
+            { "DarkElf", new[] { "DarkElfRace", "DarkElfRaceVampire" } },
+            { "Elder", new[] { "ElderRace", "ElderRaceVampire" } },
+            { "HighElf", new[] { "HighElfRace", "HighElfRaceVampire" } },
+            { "Imperial", new[] { "ImperialRace", "ImperialRaceVampire" } },
+            { "Khajiit", new[] { "KhajiitRace", "KhajiitRaceVampire" } },
+            { "Nord", new[] { "NordRace", "NordRaceVampire" } },
+            { "Orc", new[] { "OrcRace", "OrcRaceVampire" } },
+            { "Redguard", new[] { "RedguardRace", "RedguardRaceVampire" } },
+            { "SnowElf", new[] { "SnowElfRace", "SnowElfRaceVampire" } },
+            { "WoodElf", new[] { "WoodElfRace", "WoodElfRaceVampire" } },
+
+            { "Human", new[] { "Breton", "Elder", "Imperial", "Nord", "Redguard" } },
+            { "Elf", new[] { "DarkElf", "HighElf", "SnowElf", "WoodElf" } },
+            { "Humanoid", new[] { "Human", "Elf", "Orc" } },
+            { "Beast", new[] { "Argonian", "Khajiit" } },
+            { "All", new[] { "Humanoid", "Beast" } },
+
+            { "CheckDups", new[] { "Human", "Human", "Humanoid", "All" } },
+        };
+
+        [TestMethod]
+        public void TestRaceGroupConversion()
+        {
+            List<RaceGroup> raceGroups = RaceGroup.FromIdDictionary(raceIdGroups, _state.LoadOrder.PriorityOrder.Race().WinningOverrides()).ToList();
+            Assert.AreEqual(raceGroups.Count, 18);
+            Assert.AreEqual(raceGroups[17].Races.Count, 23);
+        }
+    }
+}
